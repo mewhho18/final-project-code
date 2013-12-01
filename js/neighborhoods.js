dbb7613fc91889dae074a89393919ddc64c5988b
Brett Yamada <brett.yamada@gmail.com>
2013-11-30 22:36:13 -0800

    11/30 Changed around styles a little, data structure for neighborhoods, and made greenlake.html

---
+++ js/neighborhoods.js
@@ -0,0 +1,179 @@
+/* 
+	neighborhoods.js
+*/
+
+var greenLakeEast = [
+	{
+		"vacant": false,
+		"address": "7311 Woodlawn NE",
+		"zip": "98115",
+		"unit": 6,
+		"bedroom" : 2,
+		"bath": 1
+	},
+	{
+		"vacant": false,
+		"address": "7311B Woodlawn NE",
+		"zip": "98115",
+		"unit": 6,
+		"bedroom" : 2,
+		"bath": 1
+	},
+	{
+		"vacant": false,
+		"address": "7313 Woodlawn NE",
+		"zip": "98115",
+		"unit": 6,
+		"bedroom" : 2,
+		"bath": 1
+	},
+	{
+		"vacant": false,
+		"address": "7313B Woodlawn NE",
+		"zip": "98115",
+		"unit": 6,
+		"bedroom" : 2,
+		"bath": 1
+	},
+	{
+		"vacant": false,
+		"address": "7315 Woodlawn NE",
+		"zip": "98115",
+		"unit": 6,
+		"bedroom" : 2,
+		"bath": 1
+	},
+	{
+		"vacant": false,
+		"address": "7317 Woodlawn NE",
+		"zip": "98115",
+		"unit": 6,
+		"bedroom" : 2,
+		"bath": 1
+	}, /*End of the one building*/
+	{
+		"vacant": false,
+		"address": "7319 Woodlawn NE",
+		"zip": "98115",
+		"unit": 4,
+		"bedroom" : 1,
+		"bath": 1
+	},
+	{
+		"vacant": false,
+		"address": "7321 Woodlawn NE",
+		"zip": "98115",
+		"unit": 4,
+		"bedroom" : 1,
+		"bath": 1
+	},	
+	{
+		"vacant": false,
+		"address": "7323 Woodlawn NE",
+		"zip": "98115",
+		"unit": 4,
+		"bedroom" : 1,
+		"bath": 1
+	},	
+	{
+		"vacant": false,
+		"address": "7325 Woodlawn NE",
+		"zip": "98115",
+		"unit": 4,
+		"bedroom" : 1,
+		"bath": 1
+	} /*End of the second building*/
+];
+
+var greenLakeWest = [
+	{
+		"vacant": false,
+		"address": "7103 Linden Ave N",
+		"zip": "98103",
+		"unit": 4,
+		"bedroom" : 1,
+		"bath": 1
+	} /*Apartments 1, 2, 3, 4 ?? // What about 3-bedroom 
+		single family house?*/
+];
+
+var uDistrict = [
+	{
+		"vacant": false,
+		"address": "5210 11th NE",
+		"zip": "98115",
+		"unit": 4,
+		"bedroom" : 3,
+		"bath": 1
+	} /*Plus a one unit, two bedroom, one bath? We Need Kat to clarify*/
+];
+
+var lictonSprings = [ /*Needs to specify which are 3 and 4 bedroom*/
+	{
+		"vacant": false,
+		"address": "9200 Densmore Ave N",
+		"zip": "98103",
+		"unit": 6,
+		"bedroom" : ,
+		"bath":  2
+	},
+	{
+		"vacant": false,
+		"address": "9202 Densmore Ave N",
+		"zip": "98103",
+		"unit": 6,
+		"bedroom" : ,
+		"bath": 2
+	},
+	{
+		"vacant": false,
+		"address": "9204 Densmore Ave N",
+		"zip": "98103",
+		"unit": 6,
+		"bedroom" : ,
+		"bath": 2
+	},
+	{
+		"vacant": false,
+		"address": "9206 Densmore Ave N",
+		"zip": "98103",
+		"unit": 6,
+		"bedroom" : ,
+		"bath": 2
+	},
+	{
+		"vacant": false,
+		"address": "9208 Densmore Ave N",
+		"zip": "98103",
+		"unit": 6,
+		"bedroom" : ,
+		"bath": 2
+	},
+	{
+		"vacant": false,
+		"address": "9210 Densmore Ave N",
+		"zip": "98103",
+		"unit": 6,
+		"bedroom" : ,
+		"bath": 2
+	}
+];
+
+var downtown = [ /*In a 21 story highrise*/
+	{
+		"vacant": false,
+		"address": "2100 3rd Ave #402",
+		"zip": "98121",
+		"unit": null,
+		"bedroom" : 2,
+		"bath": 2
+	},
+	{
+		"vacant": false,
+		"address": "2100 3rd Ave #1801",
+		"zip": "98121",
+		"unit": null,
+		"bedroom" : 2,
+		"bath": 2
+	} /*Condo*/
+];
